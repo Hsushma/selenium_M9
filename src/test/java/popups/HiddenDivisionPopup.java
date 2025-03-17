900bb56876420bf46a3d9af0d226fe18f89d1d76
Acer <Acer@DESKTOP-LHN9MF2>
2025-03-17 18:20:33 +0530

    popups1

--- src/test/java/popups/HiddenDivisionPopup.java
+++ src/test/java/popups/HiddenDivisionPopup.java
@@ -4,22 +4,38 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class HiddenDivisionPopup
 {
-
-
 	@Test 
 	public static void hidendivisionPopup() 
 	{
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://www.makemytrip.com/");
-		
-		
+
 		driver.findElement(By.xpath("//span[@class='commonModal__close']")).click();
+		driver.findElement(By.xpath("//span[text()='Departure']")).click();
+		//also try with for infinate loop for(; ;)
+		while(true)//while infinate loop
+		{
+			try 
+			{
+				driver.findElement(By.xpath("//div[text()='August 2025']/../..//p[text()='15']")).click();
+				break;
+			} 
+			catch (Exception e) 
+			{
+				driver.findElement(By.xpath("//span[@aria-label='Next Month']")).click();
+			}
+		}
+		driver.manage().window().minimize();
+		driver.quit();
 	}
 }
